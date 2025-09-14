e8a72b40d70b6633476a6e047c5ce339813bbe30
Мария Масленникова <mari.maslennikova.06@mail.ru>
2025-09-14 12:17:10 +0300

    added main for sphere and circle

--- main/main.cpp
+++ main/main.cpp
@@ -1,8 +1,53 @@
-// Copyright 2024 Marina Usova
+// Copyright 2025 Mary Maslennikova
 
-#define EASY_EXAMPLE
+//#define EASY_EXAMPLE
+#define CIRCLE
+//#define SPHERE
+
+#include <iostream>
+#include <clocale>
+
+enum Position { intersect, inside, no_point, match, touch };
+
+template<class T>
+Position check_position(const T& first, const T& second) {
+    int d = first.calculate_distance(second);
+    if (first.get_rad() == second.get_rad() && first.get_point() == second.get_point())
+        return match;
+    else if (d == first.get_rad() + second.get_rad())
+        return touch;
+    else if ((d < first.get_rad() - second.get_rad()) && d > 0)
+        return inside;
+    else if (d > second.get_rad() + first.get_rad())
+        return no_point;
+    else
+        return intersect;
+}
+
+void show_position(Position pos) {
+    switch (pos)
+    {
+    case intersect:
+        std::cout << "Объекты пересекаются\n";
+        break;
+    case inside:
+        std::cout << "Один объект внутри другого\n";
+        break;
+    case no_point:
+        std::cout << "Объекты не имеют общих точек\n";
+        break;
+    case match:
+        std::cout << "Объекты совпадают\n";
+        break;
+    case touch:
+        std::cout << "Объекты касаются в одной точке\n";
+        break;
+    default:
+        break;
+    }
+}
+
 #ifdef EASY_EXAMPLE
-
 #include <iostream>
 #include <iomanip>
 #include "../lib_easy_example/easy_example.h"
@@ -30,8 +75,63 @@ int main() {
   } catch (std::exception err) {
       std::cerr << err.what() << std::endl;
   }
-
   return 0;
 }
-
 #endif  // EASY_EXAMPLE
+
+#ifdef CIRCLE
+#include "../lib_circle/circle.h"
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    Point point1(5, 8);
+    Circle c1(point1, 9);
+    Circle c2(3, 8, 6);
+    show_position(check_position(c1, c2));
+    Point point2(0, 0);
+    Circle c3(point2, 3);
+    Circle c4(15, 8, 2);
+    show_position(check_position(c3, c4));
+    Point point3(0, 0);
+    Circle c5(point3, 3);
+    Circle c6(8, 0, 5);
+    show_position(check_position(c5, c6));
+    Point point4(-2, 2);
+    Circle c7(point4, 3);
+    Circle c8(8, 2, 7);
+    show_position(check_position(c7, c8));
+    Point point5(-2, 2);
+    Circle c9(point5, 3);
+    Circle c10(-2, 2, 3);
+    show_position(check_position(c9, c10));
+    return 0;
+}
+#endif  // CIRCLE
+
+#ifdef SPHERE
+#include "../lib_sphere/sphere.h"
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    Point3D point1(-2, 2, 2);
+    Sphere s1(point1, 3);
+    Sphere s2(-2, 2, 2, 3);
+    show_position(check_position(s1, s2));
+    Point3D point2(-2, 2, 2);
+    Sphere s3(point2, 3);
+    Sphere s4(-5, 8, 7, 2);
+    show_position(check_position(s3, s4));
+    Point point3(0, 3);
+    Sphere s5(point3, 5, 3);
+    Sphere s6(0, 3, 4, 2);
+    show_position(check_position(s5, s6));
+    Point point(0, 0);
+    Sphere s7(point, 0, 3);
+    Sphere s8(0, 1, 0, 1);
+    show_position(check_position(s7, s8));
+    Sphere s9(0, 0, 0, 2);
+    Sphere s10(3, 0, 0, 1);
+    show_position(check_position(s9, s10));
+    return 0;
+}
+#endif  // SPHERE
