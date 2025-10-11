2f7a2da39c9145fbdf6368b3e5fc77e3ee648ecb
Мария Масленникова <mari.maslennikova.06@mail.ru>
2025-10-11 22:23:15 +0300

    added lib_queue, lib_stack, implementation stack and covered by tests

---
+++ lib_stack/stack.h
@@ -0,0 +1,105 @@
+// Copyright 2025 Mary Maslennikova
+
+#include <cstdlib>
+#include <stdexcept>
+#include <initializer_list>
+#include <iostream>
+#include <random>
+#ifndef LIB_STACK_STACK_H_
+#define LIB_STACK_STACK_H_
+
+template <class T>
+
+class Stack {
+    T* _data;
+    int _size;
+    int _top;
+
+public:
+    Stack() = default;
+    Stack(int size);  // +
+    Stack(const Stack& other);  // +
+
+    ~Stack();
+
+    inline const int get_top() const noexcept;
+    inline const int get_size() const noexcept;
+
+    void push(const T& val);  // +
+    void pop();  // +
+    T top() const;  // +
+
+    inline bool is_empty() const noexcept;  // +
+    inline bool is_full() const noexcept;  // +
+
+    void clear() noexcept;  // +
+};
+template <class T>
+Stack<T>::Stack(int size) {
+    if (size <= 0)
+        throw std::invalid_argument("The size must be greater than zero!\n");
+    _data = new T[size];
+    _size = size;
+    _top = -1;
+}
+template <class T>
+Stack<T>::Stack(const Stack& other) {
+    if (&other == NULL)
+        throw std::invalid_argument("The object was not received!\n");
+    _size = other._size;
+    _top = other._top;
+    _data = new T[_size];
+    for (int i = 0; i <= _top; i++) {
+        _data[i] = other._data[i];
+    }
+}
+
+template <class T>
+Stack<T>::~Stack() {
+    delete[] _data;
+}
+
+template <class T>
+inline const int Stack<T>::get_top() const noexcept {
+    return _top;
+}
+template <class T>
+inline const int Stack<T>::get_size() const noexcept {
+    return _size;
+}
+
+template <class T>
+void Stack<T>::push(const T& val) {
+    if (this->is_full())
+        throw std::invalid_argument("The stack is full!\n");
+    _data[++_top] = val;
+}
+template <class T>
+void Stack<T>::pop() {
+    if (this->is_empty())
+        throw std::invalid_argument("The stack is empty!\n");
+    _top--;
+}
+template <class T>
+T Stack<T>::top() const {
+    if (this->is_empty())
+        throw std::invalid_argument("The stack is empty!\n");
+    return _data[_top];
+}
+
+template <class T>
+bool Stack<T>::is_empty() const noexcept {
+    if (_top == -1)
+        return true;
+    return false;
+}
+template <class T>
+bool Stack<T>::is_full() const noexcept {
+    return _top == _size - 1;
+}
+
+template <class T>
+void Stack<T>::clear() noexcept {
+    _top = -1;
+}
+#endif  // LIB_STACK_STACK_H_
---
+++ tests/test_stack.cpp
@@ -0,0 +1,92 @@
+// Copyright 2025 Mary Maslennikova
+
+#include <gtest/gtest.h>
+#include "../lib_stack/stack.h"
+
+TEST(TestStackLib, can_create_stack) {
+    Stack<int> stack(20);
+    EXPECT_EQ(20, stack.get_size());
+    EXPECT_EQ(-1, stack.get_top());
+}
+TEST(TestStackLib, throw_when_try_constructor_with_size) {
+    ASSERT_ANY_THROW(Stack<int>(-5));
+}
+TEST(TestStackLib, throw_when_try_copy_constructor) {
+    Stack<int>* obj = nullptr;
+    ASSERT_ANY_THROW(Stack<int> copy_obj(*obj));
+}
+TEST(TestStackLib, check_the_copy_constructor) {
+    Stack<int> st1(5);
+    st1.push(3);
+    Stack<int> st2(st1);
+    EXPECT_EQ(5, st2.get_size());
+    EXPECT_EQ(0, st2.get_top());
+    EXPECT_EQ(3, st2.top());
+}
+TEST(TestStackLib, check_push) {
+    Stack<int> stack(5);
+    stack.push(5);
+    EXPECT_EQ(5, stack.top());
+}
+TEST(TestStackLib, throw_when_try_push) {
+    Stack<int> stack(3);
+    stack.push(5);
+    stack.push(3);
+    stack.push(77);
+    ASSERT_ANY_THROW(stack.push(99));
+}
+TEST(TestStackLib, check_pop) {
+    Stack<int> stack(5);
+    stack.push(5);
+    stack.push(8);
+    stack.push(3);
+    stack.pop();
+    EXPECT_EQ(8, stack.top());
+}
+TEST(TestStackLib, throw_when_try_pop) {
+    Stack<int> stack(3);
+    ASSERT_ANY_THROW(stack.pop());
+}
+TEST(TestStackLib, check_top) {
+    Stack<int> stack(5);
+    stack.push(5);
+    stack.push(8);
+    stack.push(3);
+    EXPECT_EQ(3, stack.top());
+}
+TEST(TestStackLib, throw_when_try_top) {
+    Stack<int> stack(3);
+    ASSERT_ANY_THROW(stack.top());
+}
+TEST(TestStackLib, check_is_empty) {
+    Stack<int> stack(5);
+    EXPECT_TRUE(stack.is_empty());
+}
+TEST(TestStackLib, check_is_empty_2) {
+    Stack<int> stack(5);
+    stack.push(5);
+    stack.push(8);
+    stack.push(3);
+    EXPECT_FALSE(stack.is_empty());
+}
+TEST(TestStackLib, check_is_full) {
+    Stack<int> stack(5);
+    EXPECT_FALSE(stack.is_full());
+}
+TEST(TestStackLib, check_is_full_2) {
+    Stack<int> stack(5);
+    stack.push(5);
+    stack.push(8);
+    stack.push(3);
+    stack.push(7);
+    stack.push(15);
+    EXPECT_TRUE(stack.is_full());
+}
+TEST(TestStackLib, check_clear) {
+    Stack<int> stack(5);
+    stack.push(5);
+    stack.push(8);
+    stack.push(3);
+    stack.clear();
+    EXPECT_EQ(-1, stack.get_top());
+}
