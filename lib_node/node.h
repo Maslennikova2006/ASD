4d3a1a2689b1058fd2639993ec46047d0ab6ee04
Мария Масленникова <mari.maslennikova.06@mail.ru>
2025-10-25 23:03:54 +0300

    added a list implementation and covered with tests

---
+++ lib_node/node.h
@@ -0,0 +1,28 @@
+// Copyright 2025 Mary Maslennikova
+
+#ifndef LIB_NODE_NODE_H_
+#define LIB_NODE_NODE_H_
+
+template <class T> 
+struct Node {
+public:
+    T value;
+    Node<T>* next;
+
+    Node();
+    Node(T val, Node<T>* next_ = nullptr);
+};
+
+template <class T>
+Node<T>::Node() {
+    value = 0;
+    next = nullptr;
+}
+template <class T>
+Node<T>::Node(T val, Node<T>* next_) {
+    value = val;
+    next = next_;
+}
+
+
+#endif  // LIB_NODE_NODE_H_
