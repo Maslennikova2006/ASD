ec8d260b53aa6d0c822f2780fa5b890a38fe4942
Мария Масленникова <mari.maslennikova.06@mail.ru>
2026-03-05 17:44:45 +0300

    some changes

--- lib_polynom/polynom.cpp
+++ lib_polynom/polynom.cpp
@@ -25,77 +25,19 @@ List<Monom> Polynom::get_monoms() const noexcept {
 }
 
 Polynom& Polynom::operator+=(const Polynom& second) {
-    List<Monom> res;
-    auto it1 = _polynom.begin();
-    auto it2 = second._polynom.begin();
-
-    while (it1 != nullptr && it2 != nullptr) {
-        if (*it1 > *it2) {
-            res.push_back(*it1);
-            it1++;
-        }
-        else if (*it1 < *it2) {
-            res.push_back(*it2);
-            it2++;
-        }
-        else {
-            Monom sum = *it1 + *it2;
-            if (sum.get_coeff() != 0) {
-                res.push_back(sum);
-            }
-            it1++;
-            it2++;
-        }
+    auto it = second._polynom.begin();
+    while (it != nullptr) {
+        *this += *it;
+        it++;
     }
-
-    while (it1 != nullptr) {
-        res.push_back(*it1);
-        it1++;
-    }
-
-    while (it2 != nullptr) {
-        res.push_back(*it2);
-        it2++;
-    }
-
-    _polynom = res;
     return *this;
 }
 Polynom& Polynom::operator-=(const Polynom& second) {
-    List<Monom> res;
-    auto it1 = _polynom.begin();
-    auto it2 = second._polynom.begin();
-
-    while (it1 != nullptr && it2 != nullptr) {
-        if (*it1 > *it2) {
-            res.push_back(*it1);
-            it1++;
-        }
-        else if (*it1 < *it2) {
-            res.push_back(-(*it2));
-            it2++;
-        }
-        else {
-            Monom sub = *it1 - *it2;
-            if (sub.get_coeff() != 0) {
-                res.push_back(sub);
-            }
-            it1++;
-            it2++;
-        }
+    auto it = second._polynom.begin();
+    while (it != nullptr) {
+        *this -= *it;
+        it++;
     }
-
-    while (it1 != nullptr) {
-        res.push_back(*it1);
-        it1++;
-    }
-
-    while (it2 != nullptr) {
-        res.push_back(-(*it2));
-        it2++;
-    }
-
-    _polynom = res;
     return *this;
 }
 Polynom& Polynom::operator*=(const Polynom& second) {
@@ -135,26 +77,23 @@ Polynom& Polynom::operator*=(const double scalar) {
         _polynom.push_back(zero);
         return *this;
     }
-    List<Monom> res;
+
     auto it = _polynom.begin();
     while (it != nullptr) {
-        res.push_back((*it) * scalar);
+        *it *= scalar;
         it++;
     }
-    _polynom = res;
     return *this;
 }
 Polynom& Polynom::operator/=(const double scalar) {
     if (scalar == 0.0)
         throw std::invalid_argument("You can't divide by zero!");
 
-    List<Monom> res;
     auto it = _polynom.begin();
     while (it != nullptr) {
-        res.push_back((*it) / scalar);
+        *it /= scalar;
         it++;
     }
-    _polynom = res;
     return *this;
 }
 
@@ -173,7 +112,7 @@ Polynom Polynom::operator-() const {
     res._polynom.pop_back();
     auto it = _polynom.begin();
     while (it != nullptr) {
-        res._polynom.push_back(-(*it));
+        res += -(*it);
         it++;
     }
     return res;
