7f96da43939744064502eaa8205c103b59c44bf1
Мария Масленникова <mari.maslennikova.06@mail.ru>
2026-03-10 09:48:18 +0300

    code refactoring

--- lib_itable/itable.h
+++ lib_itable/itable.h
@@ -10,38 +10,53 @@ struct Pair {
     TFirst first;
     TSecond second;
 
-    Pair() {
-        first = TFirst();
-        second = TSecond();
-    }
-    Pair(const TFirst& f, const TSecond& s) {
-        first = f;
-        second = s;
-    }
+    Pair();
+    Pair(const TFirst& f, const TSecond& s);
 
-    Pair& operator=(const Pair& other) noexcept {
-        if (this != &other) {
-            first = other.first;
-            second = other.second;
-        }
-        return *this;
-    }
+    Pair& operator=(const Pair& other) noexcept;
 
-    bool operator==(const Pair<TFirst, TSecond>& other) const {
-        return first == other.first;
-    }
-    bool operator<(const Pair<TFirst, TSecond>& other) const {
-        return first < other.first;
-    }
-    bool operator>(const Pair<TFirst, TSecond>& other) const {
-        return first > other.first;
-    }
+    bool operator==(const Pair<TFirst, TSecond>& other) const;
+    bool operator<(const Pair<TFirst, TSecond>& other) const;
+    bool operator>(const Pair<TFirst, TSecond>& other) const;
 
-    friend std::ostream& operator<<(std::ostream& os, const Pair<TFirst, TSecond>& pair) {
-        os << pair.first << ":" << pair.second;
-        return os;
-    }
+    friend std::ostream& operator<<(std::ostream& os, const Pair<TFirst, TSecond>& pair);
 };
+template <class TFirst, class TSecond>
+Pair<TFirst, TSecond>::Pair() {
+    first = TFirst();
+    second = TSecond();
+}
+template <class TFirst, class TSecond>
+Pair<TFirst, TSecond>::Pair(const TFirst& f, const TSecond& s) {
+    first = f;
+    second = s;
+}
+
+template <class TFirst, class TSecond>
+Pair<TFirst, TSecond>& Pair<TFirst, TSecond>::operator=(const Pair& other) noexcept {
+    if (this != &other) {
+        first = other.first;
+        second = other.second;
+    }
+    return *this;
+}
+template <class TFirst, class TSecond>
+bool Pair<TFirst, TSecond>::operator==(const Pair<TFirst, TSecond>& other) const {
+    return first == other.first;
+}
+template <class TFirst, class TSecond>
+bool Pair<TFirst, TSecond>::operator<(const Pair<TFirst, TSecond>& other) const {
+    return first < other.first;
+}
+template <class TFirst, class TSecond>
+bool Pair<TFirst, TSecond>::operator>(const Pair<TFirst, TSecond>& other) const {
+    return first > other.first;
+}
+template <class TFirst, class TSecond>
+std::ostream& operator<<(std::ostream& os, const Pair<TFirst, TSecond>& pair) {
+    os << pair.first << ":" << pair.second;
+    return os;
+}
 
 template <class TKey, class TValue>
 class ITable {
